cce8cdcdbe8fb824a7d330920ec3b83cd771de20
KAWAKITA Hirofumi <hrfm0818@gmail.com>
2014-12-03 14:47:18 +0900

    Add setting files.

---
+++ config.rb
@@ -0,0 +1,7 @@
+http_path       = "."
+css_dir         = "/htdocs/public/assets/css"
+sass_dir        = "/htdocs/public/assets/css"
+images_dir      = "/htdocs/public/assets/img"
+javascripts_dir = "/htdocs/public/assets/js"
+output_style    = :expanded
+line_comments   = false
---
+++ gulpfile.js
@@ -0,0 +1,134 @@
+// ------------------------------------------------------------ //
+// --- Modules ---
+
+var gulp         = require('gulp'),
+    gutil        = require('gulp-util'),
+    connect      = require('gulp-connect'),
+    filter       = require('gulp-filter'),
+    plumber      = require('gulp-plumber'),
+    tsc          = require('gulp-tsc'),
+    uglify       = require('gulp-uglifyjs'),
+    watch        = require('gulp-watch'),
+    rsync        = require('rsyncwrapper').rsync,
+    extend       = require('util')._extend;
+
+var settings;
+
+try{
+    settings = require('./gulpfile.settings.json');
+}catch(e){
+    settings = {};
+}
+
+var destDir = "htdocs/public/assets";
+
+// ------------------------------------------------------------ //
+// --- Task ---
+
+// ------------ ------------
+// tsc
+// ------------ ------------
+
+gulp.task("tsc",function(){
+
+    if( !settings.tsc ){
+        throw "No Settings for tsc";
+    }
+
+    var _conf   = settings.tsc.default;
+    var _filter = new filter(["*","!*.d.ts"]);
+
+    return gulp.src([_conf.srcDir+'/**/*.ts','!**/*.d.ts','!**/d.ts'])
+        .pipe(plumber())
+        .pipe(tsc({
+            declaration : true,
+            out         : _conf.output + ".js",
+            tmpDir      : _conf.srcDir
+        }))
+        .pipe(gulp.dest(_conf.srcDir))
+        .pipe(_filter)
+        .pipe(gulp.dest(_conf.destDir))
+        .pipe(uglify(_conf.output+".min.js",{ outSourceMap : true, basePath : _conf.destDir } ))
+        .pipe(gulp.dest(_conf.destDir));
+
+});
+
+gulp.task("tsc:watch",function(){
+    if( !settings.tsc ){
+        throw "No Settings for tsc";
+    }
+    var _conf = settings.tsc.default;
+    gulp.watch([_conf.srcDir,"!**/*.d.ts","!**/d.ts"],["tsc"]);
+});
+
+// ------------ ------------
+// livereload
+// ------------ ------------
+
+gulp.task('connect',function(){
+    if( !settings.connect ){
+        throw "No Settings for connect";
+    }
+    return connect.server(settings.connect);
+});
+
+gulp.task('livereload',['connect'],function(){
+    if( !settings.connect || !settings.livereload ){
+        throw "No settings for livereload";
+    }
+    return gulp.watch(settings.connect.watch)
+        .on('change',function(file){
+            gulp.src(file.path).pipe(connect.reload());
+        });
+});
+
+// ------------ ------------
+// rsync
+// ------------ ------------
+
+gulp.task('rsync', function() {
+
+    if( !settings || !settings.rsync ){
+        throw "No settings for rsync";
+    }
+
+    var conf = extend({},settings.rsync);
+
+    if( !conf.args ){
+        conf.args = ["--verbose"];
+    }else if( conf.args.join(" ").indexOf("--verbose") < 0 ){
+        conf.args.push("--verbose");
+    }
+
+    rsync(conf,function(error, stdout, stderr, cmd) {
+        gutil.log(error,stdout,stderr,cmd);
+    });
+
+});
+
+gulp.task('rsync:watch', function() {
+
+    if( !settings || !settings.rsync ){
+        throw "No settings for rsync";
+    }
+
+    var conf = extend({},settings.rsync);
+
+    if( !conf.args ){
+        conf.args = ["--verbose"];
+    }else if( conf.args.join(" ").indexOf("--verbose") < 0 ){
+        conf.args.push("--verbose");
+    }
+
+    var src = conf.src + "/**/*.*"
+    src.replace(/\/\//ig,"/");
+
+    gulp.src(src).pipe(plumber()).pipe(
+        watch(src,function(){
+            rsync(conf,function(error, stdout, stderr, cmd) {
+                gutil.log(error,stdout,stderr,cmd);
+            });
+        })
+    );
+
+});
